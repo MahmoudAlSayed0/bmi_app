c6eaed88e7dd20c7c3b46905b53adf80ebcb87dc
mahmoudalsayed2504@gmail.com <mahmoudalsayed2504@gmail.com>
2023-08-20 10:20:12 +0300

    added age and slider and adjusted some logic

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,7 +12,8 @@ class Home_screen extends StatefulWidget {
 
 class _Home_screenState extends State<Home_screen> {
   bool isMale = true;
-
+  int height = 170;
+  Info info = Info(true, 170, 25, 90);
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -25,55 +26,109 @@ class _Home_screenState extends State<Home_screen> {
         backgroundColor: myprimary.shade400,
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.only(top: 20.0),
+            padding: const EdgeInsets.all(24),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Expanded(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () => setState(() {
-                          isMale = true;
-                        }),
-                        child: gender(
-                          context,
-                          text: 'male',
-                          male: isMale,
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () => setState(() {
+                            isMale = true;
+                          }),
+                          child: gender(
+                            context,
+                            text: 'male',
+                            male: isMale,
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () => setState(() {
-                          isMale = false;
-                        }),
-                        child: gender(
-                          context,
-                          text: 'female',
-                          male: isMale,
+                      SizedBox(
+                        width: 24,
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () => setState(() {
+                            isMale = false;
+                          }),
+                          child: gender(
+                            context,
+                            text: 'female',
+                            male: isMale,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Height is: $height',
+                        style: const TextStyle(
+                          color: myprimary,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Slider(
+                        value: height.toDouble(),
+                        onChanged: (value) {
+                          setState(() {
+                            height = value.round();
+                          });
+                        },
+                        max: 220,
+                        min: 150,
+                      ),
+                    ],
+                  ),
                 )),
-                Expanded(child: Row()),
-                Expanded(child: Row()),
+                SizedBox(
+                  height: 24,
+                ),
+                Expanded(
+                  child: Row(children: [
+                    Expanded(
+                        child:
+                            adjustValue(context, info: info, valueName: 'age')),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Expanded(
+                        child: adjustValue(context,
+                            info: info, valueName: 'weight')),
+                  ]),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
                 MaterialButton(
                   color: Colors.white,
                   minWidth: double.infinity,
                   padding: EdgeInsets.symmetric(vertical: 20),
                   onPressed: () {},
-                  child: Text(
+                  textColor: myprimary,
+                  child: const Text(
                     'Calculate',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  textColor: myprimary,
                 ),
               ],
             ),
@@ -96,7 +151,6 @@ class _Home_screenState extends State<Home_screen> {
 
   Widget gender(BuildContext context, {required text, required bool male}) {
     return Container(
-      margin: const EdgeInsets.all(10),
       decoration: BoxDecoration(
           color: !((male) ^ (text == 'male')) ? myprimary : Colors.white,
           borderRadius: BorderRadius.circular(20)),
@@ -118,4 +172,54 @@ class _Home_screenState extends State<Home_screen> {
       ),
     );
   }
+
+  Widget adjustValue(BuildContext context,
+      {String? valueName, required Info info}) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            valueName!,
+            style: normalTextStyle,
+          ),
+          Text(
+            (valueName == 'age' ? info.age : info.weight).toString(),
+            style: normalTextStyle,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  setState(
+                      () => valueName == 'age' ? info.age-- : info.weight--);
+                },
+                mini: true,
+                child: Icon(Icons.remove),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(
+                      () => valueName == 'age' ? info.age++ : info.weight++);
+                },
+                mini: true,
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Info {
+  bool isMale;
+  int height;
+  int age;
+  int weight;
+  Info(this.isMale, this.height, this.age, this.weight);
 }
